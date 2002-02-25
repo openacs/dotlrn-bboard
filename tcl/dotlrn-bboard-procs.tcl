a77a3f5a32d668d416aa25905fd3d4c128b2601a
Ben Adida <ben@openforce.net>
2002-02-25 14:19:41 +0000

    updated contract

--- tcl/dotlrn-bboard-procs.tcl
+++ tcl/dotlrn-bboard-procs.tcl
@@ -160,6 +160,12 @@ namespace eval dotlrn_bboard {
     }
 
     ad_proc -public remove_user {
+        user_id
+    } {
+    } {
+    }
+
+    ad_proc -public remove_user_from_community {
 	community_id
 	user_id
     } {
